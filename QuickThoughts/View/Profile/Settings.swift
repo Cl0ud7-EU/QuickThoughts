71ac14d72905e1e51d0a79a4b85afa35ec91b3b5
Cl0ud7 <agilmi00@estudiantes.unileon.es>
2023-01-23 19:59:21 +0100

    ImagePicker, Settings, Icon - Added ImagePicker - Modified Settings - Added App icon

--- QuickThoughts/View/Profile/Settings.swift
+++ QuickThoughts/View/Profile/Settings.swift
@@ -13,46 +13,84 @@ struct Settings: View {
     @ObservedObject var viewModel = SettingsViewModel()
     @State private var image = UIImage()
     @State private var showSheet = false
-    
+    @State var selectedItems: [PhotosPickerItem] = []
     
     var body: some View {
         VStack {
-            Button(action: {
-                viewModel.deleteAllCoreData()
-            }, label: {
-                Text("Delete Data")
-                    .font(.largeTitle.weight(.light))
-            })
-            .foregroundColor(.mint)
-            Text("Change photo")
+            Spacer()
+            HStack {
+                Image(uiImage: self.image)
+                .resizable()
+                .cornerRadius(50)
+                .frame(width: 100, height: 100)
+                .background(Color.black.opacity(0.2))
+                .aspectRatio(contentMode: .fill)
+                .clipShape(Circle())
+            }
+            .padding(.horizontal, 20)
+            Text("Change profile pic")
                 .font(.headline)
                 .frame(maxWidth: .infinity)
                 .frame(height: 50)
-                .background(LinearGradient(gradient: Gradient(colors: [.mint, .black]), startPoint: .top, endPoint: .bottom))
+                .background(.gray)
                 .cornerRadius(16)
                 .foregroundColor(.white)
-                .padding(.horizontal, 20)
-                .foregroundColor(.mint)
+                .padding(.horizontal, 10)
                 .onTapGesture {
                     showSheet = true
+            }
+            HStack {
+                if (image.size.width != 0)
+                {
+                    Text("Apply")
+                        .font(.headline)
+                        .frame(maxWidth: .infinity)
+                        .frame(height: 50)
+                        .background(.mint)
+                        .cornerRadius(16)
+                        .foregroundColor(.white)
+                        .padding(.horizontal, 10)
+                        .onTapGesture {
+                            Task {
+                                do
+                                {
+                                    try await changePicture()
+                                }
+                                catch
+                                {
+                                    
+                                }
+                            }
+                        }
                 }
-            Button(action: {
-            }, label: {
-                Text("Change Background Picture")
-                    .font(.largeTitle.weight(.light))
-            })
+            }
+            .padding(.bottom, 50)
             .foregroundColor(.mint)
-        }
-        HStack {
-            Image(uiImage: self.image)
-            .resizable()
-            .cornerRadius(50)
-            .frame(width: 100, height: 100)
-            .background(Color.black.opacity(0.2))
-            .aspectRatio(contentMode: .fill)
-            .clipShape(Circle())
+            VStack {
+                Button(action: {
+                    viewModel.deleteAllCoreData()
+                }, label: {
+                    Text("Sign out")
+                        .font(.headline)
+                        .frame(maxWidth: .infinity)
+                        .frame(height: 50)
+                        .background(.red)
+                        .cornerRadius(16)
+                        .foregroundColor(.white)
+                        .padding(.horizontal, 20)
+                })
+            }.frame(maxHeight: .infinity,alignment: .bottom)
+            Spacer()
         }
         .padding(.horizontal, 20)
+        .sheet(isPresented: $showSheet) {
+                // Pick an image from the photo library:
+                ImagePicker(sourceType: .photoLibrary, selectedImage: self.$image)
+        }
+    }
+    func changePicture() async throws
+    {
+        print("Picture changed!")
     }
 }
 
@@ -61,3 +99,4 @@ struct Settings_Previews: PreviewProvider {
         Settings()
     }
 }
+
